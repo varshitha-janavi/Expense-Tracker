66d0dcc53483675ef532c977d490d8bf4e127857
varshitha-janavi <varshithajanavi@gmail.com>
2025-07-15 20:07:24 +0530

    Implement authentication features with JWT, add file upload support, and configure static uploads path

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,6 @@
+const User = require('../models/User')
 const jwt = require("jsonwebtoken");
+const { read } = require('xlsx');
 
 // Generate JWT token
 const generateToken = (id) => {
@@ -6,8 +8,81 @@ const generateToken = (id) => {
 };
 
 // Register User
-exports.registerUser = async (req, res) => {};
+exports.registerUser = async (req, res) => {
+    const { fullName, email, password, profileImageUrl} = req.body;
 
-exports.loginUser = async (req, res) => {};
+    // Validation: Check for missing fields
+    if (!fullName || !email || !password){
+        return res.status(400).json({message: "All fields are required"});
+    }
 
-exports.getUserInfo = async (req, res) => {};
+    try{
+        // Check if email already exists
+        const existingUser = await User.findOne({ email });
+        if(existingUser){
+            return res.status(400).json({ message: "Email already in use" });
+        }
+
+        // Create the user
+        const user = await User.create({
+            fullName,
+            email,
+            password,
+            profileImageUrl,
+        });
+
+        res.status(201).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        });
+    }
+    catch(err){
+        res
+            .status(500)
+            .json({message: "Error registering user", error: err.message})
+    }
+};
+
+// Login User
+exports.loginUser = async (req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res.status(400).son({message : "All fields are required"});
+    }
+    try {
+        const user = await User.findOne({email});
+        if(!user || !(await user.comparePassword(password))) {
+            return res.status(400).json({ message: "Invalid Credentials"});
+        }
+
+        res.status(200).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        });
+    }
+    catch(err){
+        res
+            .status(500)
+            .json({message: "Error registering user", error: err.message});
+    }
+};
+
+// Get User Info
+exports.getUserInfo = async (req, res) => {
+    try{
+        const user = await User.findById(req.user.id).select("-password");
+
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+        }
+
+        res.status(200).json(user);
+    }
+    catch(err){
+        res
+            .status(500)
+            .json({message: "Error registering user", error: err.message});
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,11 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
 
 const app = express();
 
-// Middelware to handle CORS
+// Middleware to handle CORS
 app.use(
     cors({
         origin: process.env.CLIENT_URL || "*",
@@ -22,6 +23,10 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
